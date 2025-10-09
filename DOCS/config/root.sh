e0072a9fe768da729b52028e7dcdb1d5983580f4
ueu6969 <ueu.6969@gmail.com>
2025-10-09 21:31:22 +0700

    DOCS

---
+++ DOCS/config/root.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#by tonho dalua
+# echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/cs-69/mpv/refs/heads/master/icons/config/sshd_config;
+systemctl restart ssh;
+wget -q -O /tmp/resolv.conf https://raw.githubusercontent.com/cs-69/mpv/refs/heads/master/icons/config/resolv.conf && sudo mv /tmp/resolv.conf /etc/resolv.conf;
+systemctl restart systemd-resolved;
+clear;
+echo -e "Masukkan Password:";
+read -e pwe;
+usermod -p "$(perl -e "print crypt('$pwe', 'Q4')")" root;
+clear;
+printf "Mohon Simpan Informasi Akun VPS INI
+============================================
+Akun Root (Akun Utama)
+Ip address = $(curl -Ls http://ipinfo.io/ip)
+Username   = root
+Password   = $pwe
+============================================
+"
+apt-get update -y
+apt-get update && apt-get upgrade -y && apt dist-upgrade -y && update-grub
+
+rm /root/root.sh
+reboot
